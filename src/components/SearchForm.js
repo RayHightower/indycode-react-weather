7c871df52915d4106dbc6295495a18666105ae74
Raymond T. Hightower <rth@rayhightower.com>
2019-04-24 15:22:26 -0400

    SearchForm is only partially complete.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,39 @@
 import React, { Component } from "react";
+import store from "../utils/store";
+import { getWetherData } from "../utils/weatherApi";
 
 
 export default class SearchForm
+
+
+constructor (props);
+
+super(props);
+this.state = store.geetGlobalState():
+this.updateState = ;
+
+
+componentWillMount() {
+  store.subscribe(this.updateState);
+}
+
+componentWillDisMount() {
+  store.unsubscribe(this.updateState);
+}
+
+handleSearchChange(event) {
+  store.upateGlobalState({searchTerm: event.target.value});
+}
+
+render() {
+  return (
+    <div>
+    <input type="text" onChange={this.handleSearchChange} value={this.state.searchTerm}/>
+    <button onClick={this.handleSearch}>Search</button>
+    </div>
+  )
+}
+
+
+
+
