a539bff4982dc7293efe866a1b4877ec5cdcd723
Raymond T. Hightower <rth@rayhightower.com>
2019-04-24 11:47:37 -0400

    Include updates for Part7

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import Home from "./components/Home";
 import Callback from "./components/Callback";
 import Unauthorized from "./components/Unauthorized";
 import Weather from "./components/Weather";
+import Navbar from "./components/Navbar";
+
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Weather />
+        <Router>
+          <Navbar/>
+          <Route exact path="/" component={Home} />
+          <Route path="/weather" component={Weather} />
+          <Route path="/callback" component={Callback} />
+          <Route path="/unauthorized" component={Unauthorized} />
+        </Router>
       </div>
     );
   }
